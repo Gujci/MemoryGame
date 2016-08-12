1b97da97ecda5e66b4d2d39d96819f42d09f7c84
Mate Gujgiczer <mate.gujgiczer@icloud.com>
2016-08-12 15:15:09 +0200

    Initial Commit

---
+++ MemoryGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryGame
+//
+//  Created by Gujgiczer Máté on 12/08/16.
+//  Copyright © 2016 gujci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
